a7f5c33aa14c97d4b8299bed9e171f9fe4f284fb
nafisanowshad <nafisanowshadmithila@gmail.com>
2026-03-29 23:56:31 +0600

    all necessary package added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {} from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h2 className='text-red-700 bg-amber-400 font-bold text-2xl p-5'>Hello World!</h2>
+    </>
+  )
+}
+
+export default App
