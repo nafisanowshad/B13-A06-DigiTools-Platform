64e70124f5cb4f8153f31cb6aabc16651570a3e7
nafisanowshad <nafisanowshadmithila@gmail.com>
2026-03-30 21:57:27 +0600

    banner section responsiveness and navbar section responsiveness added

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,55 +1,55 @@
 import React from "react";
-import bannerLogo from "../../assets/bannerLogo.png";
-import bannerimg from "../../assets/banner.png";
-import { IoPlayOutline } from "react-icons/io5";
+import bannerImage from "../../assets/banner.png";
+import playImage from "../../assets/Play.png";
 
 const Banner = () => {
   return (
-    <div className="w-10/12 mx-auto mt-15 grid grid-cols-2">
-      <div className="space-y-7">
-        <button className="flex items-center gap-1 p-2 px-4 border-none shadow-none btn rounded-full !bg-[#e1e7ff]">
-          <img src={bannerLogo} alt="" className="w-4 h-4 rounded-full" />
+    <div className="hero  min-h-screen container mx-auto pt-10 pb-20">
+      <div className="hero-content justify-between flex-col-reverse lg:flex-row-reverse gap-10 mt-6 md:mt-10 lg:mt-5">
+        <div className="flex-1">
+          <img
+            src={bannerImage}
+            alt="banner"
+            className=" rounded-lg shadow-md"
+          />
+        </div>
+        <div className="flex-1 space-y-2 flex flex-col items-center lg:items-start text-center lg:text-left">
+          <div className="flex items-center w-fit gap-3 px-6 py-3 bg-linear-to-r from-purple-100 to-indigo-100 rounded-full">
+            <div className="relative flex items-center justify-center">
+              <div className="w-5 h-5 bg-purple-300 rounded-full absolute opacity-40"></div>
 
-          <span className="bg-linear-to-b from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent font-medium text-[13px]">
-            New: AI-Powered Tools Available
-          </span>
-        </button>
+              <div className="w-3.5 h-3.5 bg-purple-400 rounded-full absolute"></div>
 
-        <div>
-          <h1 className="text-[#101727FF] text-[70px] font-bold">
-            Supercharge Your Digital Workflow
+              <div className="w-2 h-2 bg-purple-600 rounded-full relative animate-pulse"></div>
+            </div>
+
+            <span className="gradient-text font-medium text-sm">
+              New: AI-Powered Tools Available
+            </span>
+          </div>
+          <h1 className=" text-3xl md:text-4xl lg:text-6xl font-extrabold leading-normal">
+            Supercharge Your <span className="gradient-text">Digital Workflow</span>
           </h1>
-          <p className="text-[#627382FF] font-normal leading-7">
-            Access premium AI tools, design assets, templates, and productivity
+          <p className="py-6 text-black/50 leading-normal">
+            Access premium AI tools, design assets, templates, and productivity{" "}
+            <br />
             software—all in one place. Start creating faster today. <br />
             Explore Products
           </p>
-        </div>
-        <div className="flex gap-7">
-          <button className="bg-linear-to-b from-[#4F39F6] to-[#9514FA] py-2 px-3 rounded-full hover:opacity-90 transition-all duration-300 hover:-translate-y-1 cursor-pointer font-semibold text-[16px]">
-            Explore Products
-          </button>
-
-          {/* <div className="flex gap-1 justify-center items-center">
-          <div><IoPlayOutline /></div>
-          <button className="bg-linear-to-b from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent"> Watch Demo</button>
-          </div> */}
-          <button className="group relative p-[1.5px] rounded-full bg-linear-to-b from-[#4F39F6] to-[#9514FA]">
-            <div className="flex items-center justify-center gap-2 px-4 py-2 bg-white rounded-full transition-all duration-300 hover:-translate-y-1 cursor-pointer hover:border-3 border-blue-700">
-              <div className="text-[#4F39F6] text-xl">
-                <IoPlayOutline />
+          <div className="flex flex-col sm:flex-row items-center gap-4">
+            <button className="btn text-white btn-primary rounded-full gradient-bg hover:shadow-lg hover:shadow-indigo-400/50 transition duration-300">
+              Explore Products
+            </button>
+            <button className="btn bg-transparent border-none p-0 ">
+              <div className="p-0.5 rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] hover:shadow-xl hover:shadow-indigo-400/50 transition duration-300">
+                <div className="flex items-center gap-2 px-4 py-2 bg-white rounded-full">
+                  <img  src={playImage} alt="video play icon" />
+                  <span className="gradient-text ">Watch Demo</span>
+                </div>
               </div>
-
-              <span className="font-semibold text-[16px] bg-linear-to-b from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">
-                Watch Demo
-              </span>
-            </div>
-          </button>
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div>
-        <img src={bannerimg} alt="" className="w-full object-cover" />
       </div>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,59 +1,99 @@
-import React, { useState } from "react";
-import { FiShoppingCart, FiMenu, FiX } from "react-icons/fi";
-import navlogo from "../../assets/navlogo.png";
+import React from "react";
+import shoppingImage from "../../assets/products/shopping-cart.png";
 
-const Navbar = () => {
-  const [open, setOpen] = useState(false);
+const NavBar = ({carts}) => {
   return (
-    <nav className="relative">
-      <div className="w-10/12 mx-auto p-7 items-center flex justify-between">
-        <div className="flex items-center gap-3">
-          <div
-            className="lg:hidden text-2xl text-black cursor-pointer"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <FiX /> : <FiMenu />}
+    <div className=" bg-base-100 shadow-sm">
+      <div className="container mx-auto navbar ">
+        <div className="navbar-start ">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow font-semibold text-black/80"
+            >
+              <li>
+                <a>Products</a>
+              </li>
+              <li>
+                <a>Features</a>
+              </li>
+              <li>
+                <a>Pricing</a>
+              </li>
+              <li>
+                <a>Testimonials</a>
+              </li>
+              <li>
+                <a>FAQ</a>
+              </li>
+            </ul>
           </div>
-          <img src={navlogo} alt="" className="w-32 md:w-auto" />
+          <h1 className="leading-[1.3] text-xl md:text-2xl lg:text-4xl font-extrabold  gradient-text">
+            DigiTools
+          </h1>
         </div>
-
-        <div className="hidden lg:block">
-          <ul className="flex gap-9 font-semibold text-[#101727]">
-            <li>Products</li>
-            <li>Features</li>
-            <li>Pricing</li>
-            <li>Testimonials</li>
-            <li>FAQ</li>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 font-semibold text-black/80">
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>Features</a>
+            </li>
+            <li>
+              <a>Pricing</a>
+            </li>
+            <li>
+              <a>Testimonials</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
           </ul>
         </div>
+        <div className="navbar-end">
+          <div className="flex items-center justify-between gap-4 px-3">
+           
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost "
+            >
+              <div className="indicator">
+                <img src={shoppingImage} alt="shopping cart" />
+                {
+                  carts.length > 0 && <span className="badge badge-sm indicator-item btn-circle rounded-full text-white bg-red-500">{carts.length}</span> 
+                }
+              </div>
+            </div>
 
-        <div className="flex gap-4 justify-center items-center font-semibold text-[#101727]">
-          <div>
-            <FiShoppingCart />
-          </div>
-          <div className="hidden sm:block cursor-pointer">Login</div>
-          <div>
-            <button className="px-5 py-2 md:px-7 md:py-3  btn rounded-full font-semibold text-[#FFFFFF] bg-linear-to-r from-[#4F39F6] to-[#9514FA] border-none transition-all duration-300 hover:-translate-y-1 hover:opacity-90 hover:shadow-lg cursor-pointer">
+            <p className="font-semibold text-black/80">Login</p>
+
+            <a className="btn rounded-full gradient-bg text-white hidden sm:flex hover:shadow-lg hover:shadow-indigo-400/50 transition duration-300">
               Get Started
-            </button>
+            </a>
           </div>
         </div>
       </div>
-
-      <div
-        className={`lg:hidden absolute left-0 w-full bg-white shadow-lg transition-all duration-300 z-50 ${open ? "top-17.5 opacity-100" : "-top-75 opacity-0"}`}
-      >
-        <ul className="flex flex-col gap-5 p-6 font-semibold text-[#101727] border-t">
-          <li>Products</li>
-          <li>Features</li>
-          <li>Pricing</li>
-          <li>Testimonials</li>
-          <li>FAQ</li>
-          <li className="sm:hidden">Login</li>
-        </ul>
-      </div>
-    </nav>
+    </div>
   );
 };
 
-export default Navbar;
+export default NavBar;
